04e098bab6e4946825db5850b6b9a5099e639a5b
Igor Ejov <maxejj@gmail.com>
2022-03-31 12:23:58 +0300

    up

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,62 @@
 <script>
 
+import { onMount } from "svelte";
+  const endpoint = "http://chat/api/todo";
+  let todoList = [];
+
+  onMount(async function () {
+    const response = await fetch(endpoint);
+    todoList = await response.json();
+  });
+
 	let valueInput = '';
 
-	let toDoDefault = [{
-			value: 'First to do task',
-			status: true
-		},
-		{
-			value: 'Second to do task',
-			status: false
-		},
-		{
-			value: 'Third to do task',
-			status: true
-		},
-	]
+	async function deleteElement(id,index){
+		const res = await fetch('http://chat/api/todo/delete', {
+			method: 'POST',
+			body: JSON.stringify({
+				id
+			})
+		})
+		
+		const json = await res.json()
 
+		console.log(json);
 
-	function deleteElement(index){
-		toDoDefault.splice(index, 1)
-		toDoDefault = toDoDefault;
+		todoList.splice(index, 1)
+		todoList = todoList;
 	}
 
-	function createNewEl(){
-		toDoDefault = [...toDoDefault, {value: valueInput, status: false}];
+	async function createNewEl(){
+		const res = await fetch('http://chat/api/todo/create', {
+			method: 'POST',
+			body: JSON.stringify({
+				valueInput,
+				status:'false',
+			})
+		})
+		
+		const json = await res.json()
+	
+		console.log(json);
+		
+		todoList = [json,...todoList];
 		valueInput = '';
 	}
+
+	async function changeStatus(index,id){
+		todoList[index].status = !todoList[index].status;
+		const res = await fetch('http://chat/api/todo/changestatus', {
+			method: 'POST',
+			body: JSON.stringify({
+				id,
+				status:todoList[index].status,
+			})
+		})
+
+	}
+
+
 
 </script>
 
@@ -38,12 +69,11 @@
 
 	<div class="w-1/3 mx-auto border rounded px-4 py-4 mt-10">
 		<ul>
-			{#each toDoDefault as el,index}
-			<li class="flex align-center justify-between px-4 mb-4 py-2 border rounded transition"
-			 class:active = {el.status}> 
-				<span class="text-lg cursor-pointer hover:text-sky-600" on:click="{()=> el.status = !el.status}">{el.value}</span>
+			{#each todoList as el,index}
+			<li class="{el.status == true ? 'active' : ''} flex align-center justify-between px-4 mb-4 py-2 border rounded transition"> 
+				<span class="text-lg cursor-pointer hover:text-sky-600" on:click="{()=>changeStatus(index,el.id)}">{el.value}</span>
 				<div class="delete px-2 cursor-pointer bg-rose-600 hover:bg-rose-800 transition
-					text-white flex justify-center items-center rounded" on:click={()=>deleteElement(index)}>
+					text-white flex justify-center items-center rounded" on:click={()=>deleteElement(el.id,index)}>
 					
 					<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
 						<path d="M1 1.00006L11 11.0001M1 11.0001L11 1.00006" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -52,6 +82,7 @@
 				</div>
 			</li>
 			{/each}
+			
 		</ul>
 	</div>
 
@@ -65,19 +96,6 @@
 		opacity: 0.8;
 		background-color: #eee;
 		position: relative;
-		margin-left: 30px;
-		transition: 0.4s;
-	}
-	li.active::before {
-		content: '';
-		position: absolute;
-		right: calc(100% + 15px);
-		top: 50%;
-		transform: translateY(-50%);
-		width: 10px;
-		height: 10px;
-		background-color: rgb(42, 161, 42);
-		border-radius: 50%;
 		transition: 0.4s;
 	}
 </style>
